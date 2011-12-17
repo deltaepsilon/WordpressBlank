811b3aaaaabcd370e0d9e302a169019917293502
Chris <christopher.esplin@gmail.com>
2011-12-17 00:06:02 -0700

    Post CSS

--- index.php
+++ index.php
@@ -13,19 +13,28 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-
+			
 			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+			
+			<div class="post-banner"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></div>
+
+			
 
 			<div class="entry">
 				<?php the_content(); ?>
 			</div>
 
 			<div class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+				<ul>
+					<li class="share-popup"><a href="#">share</a></li>
+					<li class="comment-count"><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></li>
+					<li class="read-more-image"><a href="<?php the_permalink() ?>"><div class="read-more"></div></a></li>
+				</ul>
+				<span></span>
+				<!-- <?php the_tags('Tags: ', ', ', '<br />'); ?> -->
+				<div class="posted-in">
+					Posted in: <?php the_category(', ') ?>
+				</div>
 			</div>
 
 		</div>
