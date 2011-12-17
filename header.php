baa1dcc78915f1d401ff9bad118735bf95806f15
Chris <christopher.esplin@gmail.com>
2011-12-16 23:07:23 -0700

    Initial layout

--- header.php
+++ header.php
@@ -35,7 +35,6 @@
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>/css/ie.css" type="text/css" />
 	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
@@ -52,5 +51,19 @@
 		<div id="header">
 			<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
 			<div class="description"><?php bloginfo('description'); ?></div>
+			<div id="header-image" class="sweet-muffin-large"></div>
+			<div id="social-buttons">
+				<div class="find-me-here"></div>
+				<ul>
+					<li><a href="facebook.com"><div class="facebook"></div></a></li>
+					<li><a href="facebook.com"><div class="twitter"></div></a></li>
+					<li><a href="facebook.com"><div class="pinterest"></div></a></li>
+					<li><a href="/?feed=rss2"><div class="rss"></div></a></li>
+					<li><a href="facebook.com"><div class="email"></div></a></li>
+					<li><a href="facebook.com"><div class="shopping-cart"></div></a></li>
+				</ul> 
+			</div>
+			<div class="see-my-work"></div>
+			<div class="shop-badge"></div>
 		</div>
 
