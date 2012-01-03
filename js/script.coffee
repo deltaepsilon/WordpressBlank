2ab4ce4c307649eff28745aeafaf4142a0a46c1c
Chris <christopher.esplin@gmail.com>
2012-01-02 19:16:36 -0700

    Finished layout and forms

--- js/script.coffee
+++ js/script.coffee
@@ -4,8 +4,7 @@ window.frugal =
   # linksPadding: 0
   # pageWidth: 0
   # pagePadding: 0
-  layout: ->
-    
+  layout: ->    
     navLinks = $('#frugal-nav')
     ul = navLinks.children('ul')
     navLinksItems = $('#frugal-nav li')
@@ -15,7 +14,6 @@ window.frugal =
       linksItemsMargin = parseInt($('#frugal-nav li').css('margin-left'))
       linksItemsPadding = parseInt($('#frugal-nav li').css('padding-left'))
       navLinksItems.each ->
-        console.log $(this).width() + 2 * ( linksItemsMargin + linksItemsPadding )
         window.frugal.linksItemsWidth += $(this).width() + 2 * ( linksItemsMargin + linksItemsPadding )
     finally
       ul.css 'margin-left', ( window.frugal.linksWidth - window.frugal.linksItemsWidth ) / 2
@@ -28,8 +26,44 @@ window.frugal =
       marginTop: -2
     facebook.css 
       width: 80
+  contactLink: ->
+    link = $('.contact-link')
+    form = $('#contact-form')
+    wrapper = $('#post-wrapper')
+    wrapper.addClass 'closed'
+    $('#form-message textarea').attr 'cols', 125
+    link.click (e) ->
+      height = form.height() + (2 * parseInt(form.css('padding-top'))) + 47
+      console.log form.height()
+      console.log (2 * parseInt(form.css('padding-top')))
+      console.log height
+      e.preventDefault()
+      if $('.open').length >= 1
+        return
+      link.addClass 'closer'
+      wrapper.addClass 'open'
+      wrapper.animate
+        marginTop: '+=' + height
+      ,->
+        form.css 'z-index', 1
+        $('.closer').one 'click', ->
+          $('.closer').removeClass 'closer'
+          if $('.open').length < 1
+            return
+          wrapper.animate
+            marginTop: 0
+          form.css 'z-index', -1
+          wrapper.removeClass('open').addClass('closed')
+    clickCloser = ->
+        $('.closer').trigger 'click'
+    $('#form-close').click ->
+      setTimeout clickCloser, 200
+    $('#form-submit input').click ->
+      if $('.wpcf7-mail-sent-ok').length >= 1
+        setTimeout clickCloser, 2000
 $(document).ready ->
   console.log "We're ready snitches."
   window.frugal.layout()
   window.frugal.shareButtons()
+  window.frugal.contactLink()
   console.log window.frugal
--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@
         linksItemsMargin = parseInt($('#frugal-nav li').css('margin-left'));
         linksItemsPadding = parseInt($('#frugal-nav li').css('padding-left'));
         return navLinksItems.each(function() {
-          console.log($(this).width() + 2 * (linksItemsMargin + linksItemsPadding));
           return window.frugal.linksItemsWidth += $(this).width() + 2 * (linksItemsMargin + linksItemsPadding);
         });
       } finally {
@@ -32,12 +31,61 @@
       return facebook.css({
         width: 80
       });
+    },
+    contactLink: function() {
+      var clickCloser, form, link, wrapper;
+      link = $('.contact-link');
+      form = $('#contact-form');
+      wrapper = $('#post-wrapper');
+      wrapper.addClass('closed');
+      $('#form-message textarea').attr('cols', 125);
+      link.click(function(e) {
+        var height;
+        height = form.height() + (2 * parseInt(form.css('padding-top'))) + 47;
+        console.log(form.height());
+        console.log(2 * parseInt(form.css('padding-top')));
+        console.log(height);
+        e.preventDefault();
+        if ($('.open').length >= 1) {
+          return;
+        }
+        link.addClass('closer');
+        wrapper.addClass('open');
+        return wrapper.animate({
+          marginTop: '+=' + height
+        }, function() {
+          form.css('z-index', 1);
+          return $('.closer').one('click', function() {
+            $('.closer').removeClass('closer');
+            if ($('.open').length < 1) {
+              return;
+            }
+            wrapper.animate({
+              marginTop: 0
+            });
+            form.css('z-index', -1);
+            return wrapper.removeClass('open').addClass('closed');
+          });
+        });
+      });
+      clickCloser = function() {
+        return $('.closer').trigger('click');
+      };
+      $('#form-close').click(function() {
+        return setTimeout(clickCloser, 200);
+      });
+      return $('#form-submit input').click(function() {
+        if ($('.wpcf7-mail-sent-ok').length >= 1) {
+          return setTimeout(clickCloser, 2000);
+        }
+      });
     }
   };
   $(document).ready(function() {
     console.log("We're ready snitches.");
     window.frugal.layout();
     window.frugal.shareButtons();
+    window.frugal.contactLink();
     return console.log(window.frugal);
   });
 }).call(this);
